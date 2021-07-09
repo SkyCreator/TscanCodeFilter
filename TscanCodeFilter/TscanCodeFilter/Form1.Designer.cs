f6c9e0c0eb38da5f4ea8f83d1355318e1cc5675f
zhaolei <504230210@qq.com>
2021-07-09 15:25:01 +0800

    Add files via upload

---
+++ TscanCodeFilter/TscanCodeFilter/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace TscanCodeFilter
+{
+    partial class TscanCodeFilter
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openBtn = new System.Windows.Forms.Button();
+            this.filterBtn = new System.Windows.Forms.Button();
+            this.showxml = new System.Windows.Forms.Label();
+            this.exportxml = new System.Windows.Forms.Label();
+            this.versionIdTB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openBtn
+            // 
+            this.openBtn.Location = new System.Drawing.Point(44, 36);
+            this.openBtn.Name = "openBtn";
+            this.openBtn.Size = new System.Drawing.Size(194, 23);
+            this.openBtn.TabIndex = 0;
+            this.openBtn.Text = "打开TscanCode生成xml文件";
+            this.openBtn.UseVisualStyleBackColor = true;
+            this.openBtn.Click += new System.EventHandler(this.openBtn_Click);
+            // 
+            // filterBtn
+            // 
+            this.filterBtn.Location = new System.Drawing.Point(44, 125);
+            this.filterBtn.Name = "filterBtn";
+            this.filterBtn.Size = new System.Drawing.Size(194, 23);
+            this.filterBtn.TabIndex = 1;
+            this.filterBtn.Text = "过滤并生成新xml文件";
+            this.filterBtn.UseVisualStyleBackColor = true;
+            this.filterBtn.Click += new System.EventHandler(this.filterBtn_Click);
+            // 
+            // showxml
+            // 
+            this.showxml.AutoSize = true;
+            this.showxml.Location = new System.Drawing.Point(12, 84);
+            this.showxml.MaximumSize = new System.Drawing.Size(260, 40);
+            this.showxml.Name = "showxml";
+            this.showxml.Size = new System.Drawing.Size(0, 12);
+            this.showxml.TabIndex = 2;
+            // 
+            // exportxml
+            // 
+            this.exportxml.AutoSize = true;
+            this.exportxml.Location = new System.Drawing.Point(12, 200);
+            this.exportxml.MaximumSize = new System.Drawing.Size(260, 40);
+            this.exportxml.Name = "exportxml";
+            this.exportxml.Size = new System.Drawing.Size(0, 12);
+            this.exportxml.TabIndex = 3;
+            // 
+            // versionIdTB
+            // 
+            this.versionIdTB.Location = new System.Drawing.Point(148, 9);
+            this.versionIdTB.Name = "versionIdTB";
+            this.versionIdTB.Size = new System.Drawing.Size(124, 21);
+            this.versionIdTB.TabIndex = 4;
+            this.versionIdTB.Text = "0";
+            this.versionIdTB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "请输入要过滤版本号:";
+            // 
+            // TscanCodeFilter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.versionIdTB);
+            this.Controls.Add(this.exportxml);
+            this.Controls.Add(this.showxml);
+            this.Controls.Add(this.filterBtn);
+            this.Controls.Add(this.openBtn);
+            this.Name = "TscanCodeFilter";
+            this.Text = "TscanCodeFilter";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.TscanCodeFilter_Close);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openBtn;
+        private System.Windows.Forms.Button filterBtn;
+        private System.Windows.Forms.Label showxml;
+        private System.Windows.Forms.Label exportxml;
+        private System.Windows.Forms.TextBox versionIdTB;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TscanCodeFilter/TscanCodeFilter/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Diagnostics;
+using System.Collections;
+using System.Threading;
+using System.IO;
+namespace TscanCodeFilter
+{
+    public partial class TscanCodeFilter : Form
+    {
+        private FileStream m_LogFile = null;
+        private StreamWriter m_LogSW = null;
+        private string m_FilePath = "";
+        private uint m_VersionId = 0;
+        private XmlDocument m_LoadXmlDoc = new XmlDocument();
+        public class FilterNode
+        {
+            public string line;
+            public string severity;
+            public bool isFilter = false;
+        }
+        private Dictionary<string, List<FilterNode>> m_FilterNodeDic = new Dictionary<string, List<FilterNode>>();
+        private List<string> m_Keys = null;
+        public TscanCodeFilter()
+        {
+            InitializeComponent();
+        }
+
+        private void openBtn_Click(object sender, EventArgs e)
+        {
+            if (m_LogSW != null)
+            {
+                m_LogSW.Close();
+                m_LogSW = null;
+            }
+            if (m_LogFile != null)
+            {
+                m_LogFile.Close();
+                m_LogFile = null;
+            }
+            if (m_Keys != null)
+            {
+                m_Keys.Clear();
+            }
+            if (m_FilterNodeDic != null)
+            {
+                m_FilterNodeDic.Clear();
+            }
+            if (File.Exists("Log.txt"))
+            {
+                File.Delete("Log.txt");
+            }
+            if (Directory.Exists("Tmp"))
+            {
+                Directory.Delete("Tmp", true);
+            }
+            m_LogFile = new FileStream("Log.txt", FileMode.OpenOrCreate);
+            m_LogSW = new StreamWriter(m_LogFile);
+            string path = string.Empty;
+            var openFileDialog = new OpenFileDialog()
+            {
+                Filter = "Files (*.xml)|*.xml"//如果需要筛选txt文件（"Files (*.txt)|*.txt"）
+            };
+            DialogResult result = openFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                path = openFileDialog.FileName;
+                showxml.Text = path;
+                m_FilePath = path;
+                m_LoadXmlDoc.Load(m_FilePath);
+                LoadXmlData();
+            }
+        }
+        private void LoadXmlData()
+        {
+            XmlElement element = m_LoadXmlDoc.DocumentElement;
+            XmlNodeList nodes = element.ChildNodes;
+            foreach (var node in nodes)
+            {
+                XmlElement child = (XmlElement)node;
+                string file = child.GetAttribute("file").ToString();
+                FilterNode fn = new FilterNode();
+                fn.line = child.GetAttribute("line").ToString();
+                fn.severity = child.GetAttribute("severity").ToString();
+                if (!m_FilterNodeDic.ContainsKey(file))
+                {
+                    List<FilterNode> nodeList = new List<FilterNode>();
+                    nodeList.Add(fn);
+                    m_FilterNodeDic.Add(file, nodeList);
+                }
+                else
+                {
+                    m_FilterNodeDic[file].Add(fn);
+                }
+                int pos = file.LastIndexOf("\\");
+                string fileName = file.Substring(pos + 1);
+                exportxml.Text = string.Format("加载xml数据文件：{0}！", fileName);
+                m_LogSW.WriteLine(exportxml.Text);
+            }
+            exportxml.Text = string.Format("加载完成！加载xml数据{0}行！涉及到的代码文件{1}个！", nodes.Count, m_FilterNodeDic.Count);
+            m_LogSW.WriteLine(exportxml.Text);
+            //m_LogSW.Close();
+        }
+
+        private async void filterBtn_Click(object sender, EventArgs e)
+        {
+            this.filterBtn.Enabled = false;
+            if (!Directory.Exists("Tmp"))
+            {
+                Directory.CreateDirectory("Tmp");
+            }
+            string versionStr = versionIdTB.Text;
+            if (!uint.TryParse(versionStr, out m_VersionId))
+            {
+                MessageBox.Show("文本框需要输入一个版本号，正整数！");
+                return;
+            }
+            try
+            {
+                m_Keys = new List<string>(m_FilterNodeDic.Keys);
+
+                Process m_Process = new Process();
+                m_Process.StartInfo.FileName = "cmd.exe";  //设置要启动的应用程序
+                m_Process.StartInfo.UseShellExecute = false;  //是否使用操作系统shell启动
+                m_Process.StartInfo.RedirectStandardInput = true;  // 接受来自调用程序的输入信息
+                m_Process.StartInfo.RedirectStandardOutput = false;  //输出信息
+                m_Process.StartInfo.RedirectStandardError = true;  // 输出错误
+                m_Process.StartInfo.CreateNoWindow = true;  //不显示程序窗口
+                m_Process.Start();  //启动程序
+                string command = "";
+                List<string> outPutList = new List<string>();
+                for (int i = 0; i < m_Keys.Count; ++i)
+                {
+                    command = GetWriteCommand(m_Keys[i]);
+                    await m_Process.StandardInput.WriteLineAsync(command);
+                    string name = GetFileNameFromAllName(m_Keys[i]);
+                    exportxml.Text = string.Format("{0}:生成临时文件{1}！", i, name);
+                    await m_Process.StandardInput.FlushAsync();
+                }
+                AnalysisFilter();
+                FilterResult();
+                this.filterBtn.Enabled = true;
+            }
+            catch (Exception exception)
+            {
+                //m_LogSW = new StreamWriter(m_LogFile);
+                m_LogSW.WriteLine(exception.Message);
+            }
+        }
+        private string GetWriteCommand(string srcFile)
+        {
+            string file = srcFile;
+            int pos = file.LastIndexOf("\\");
+            string fileName = file.Substring(pos + 1);
+            string[] fileNameSplit = fileName.Split('.');
+            string command = string.Format("svn blame {0} --xml>>Tmp/{1}.xml", srcFile, fileNameSplit[0]+ fileNameSplit[1]);
+            //string command = string.Format("svn blame {0}>>Tmp/{1}.xml", srcFile, fileNameSplit[0] + fileNameSplit[1]);
+            return command;
+        }
+        private string GetFileNameFromAllName(string srcFile)
+        {
+            string file = srcFile;
+            int pos = file.LastIndexOf("\\");
+            string fileName = file.Substring(pos + 1);
+            return fileName;
+        }
+
+        private void AnalysisFilter()
+        {
+            try
+            {
+                for (int i = 0; i < m_FilterNodeDic.Count; ++i)
+                {
+                    string file = m_Keys[i];
+                    int pos = file.LastIndexOf("\\");
+                    string fileName = file.Substring(pos + 1);
+                    string[] fileNameSplit = fileName.Split('.');
+                    string xmlName = string.Format("Tmp/{0}.xml", fileNameSplit[0] + fileNameSplit[1]);
+                    List<FilterNode> list = m_FilterNodeDic[file];
+                    XmlDocument document = new XmlDocument();
+                    document.Load(xmlName);
+                    for (int j = 0; j < list.Count; ++j)
+                    {
+                        FilterNode fn = list[j];
+
+                        if (IsFilter(document, xmlName, fn.line))
+                        {
+                            fn.isFilter = true;
+                        }
+                    }
+                    exportxml.Text = string.Format("{0}：分析{1}完成！", i, xmlName);
+                    m_LogSW.WriteLine(exportxml.Text);
+                }
+                exportxml.Text = string.Format("分析完成！分析xml文件{0}个！", m_FilterNodeDic.Count);
+                m_LogSW.WriteLine(exportxml.Text);
+            }
+            catch(Exception e)
+            {
+                m_LogSW.WriteLine(e.Message);
+            }
+        }
+        
+        private bool IsFilter(XmlDocument document, string xmlName, string line)
+        {
+            XmlNodeList nodes = document.SelectNodes("blame/target/entry");
+            for (int i = 0; i < nodes.Count; ++i)
+            {
+                XmlElement child = (XmlElement)nodes[i];
+                string tmpLine = child.GetAttribute("line-number").ToString();
+                XmlElement node = (XmlElement)child.SelectSingleNode("commit");
+                if ( node == null )continue;
+                string revision = node.GetAttribute("revision").ToString();
+                uint revisionId = 0;
+                if (tmpLine == line && uint.TryParse(revision, out revisionId) )
+                {
+                    if (revisionId > m_VersionId)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        private void FilterResult()
+        {
+            XmlDocument document = new XmlDocument();
+            document.Load(m_FilePath);
+            XmlElement element = document.DocumentElement;
+            XmlNodeList nodes = element.ChildNodes;
+            List<XmlNode> delNodeList = new List<XmlNode>();
+            for (int i = 0; i < nodes.Count; ++i)
+            {
+                XmlElement child = (XmlElement)nodes[i];
+                string file = child.GetAttribute("file").ToString();
+                string line = child.GetAttribute("line").ToString();
+                string severity = child.GetAttribute("severity").ToString();
+                List<FilterNode> list = m_FilterNodeDic[file];
+                for (int j = 0; j < list.Count; ++j)
+                {
+                    if (list[j].isFilter && list[j].line == line)
+                    {
+                        delNodeList.Add(nodes[i]);
+                        break;
+                    }
+                }
+            }
+            foreach (XmlNode node in delNodeList)
+            {
+                element.RemoveChild(node);
+            }
+            int pos = m_FilePath.LastIndexOf("\\");
+            string fileName = m_FilePath.Substring(pos + 1);
+            string finalName = string.Format("Result_{0}", fileName);
+            document.Save(finalName);
+            exportxml.Text = string.Format("导出{0}完成!", finalName);
+            m_LogSW.WriteLine(exportxml.Text);
+        }
+
+        private void TscanCodeFilter_Close(object sender, FormClosedEventArgs e)
+        {
+            if (m_LogSW != null)
+            {
+                m_LogSW.Close();
+            }
+        }
+    }
+}
